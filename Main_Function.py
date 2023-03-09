4f087a13bea3e824746a1d18c39fbff0dd421d8f
mairakhann <103566481+mairakhann@users.noreply.github.com>
2023-03-09 18:57:05 +0100

    Add files via upload

--- Main_Function.py
+++ Main_Function.py
@@ -16,8 +16,9 @@ import Supporting_Function
 def Model_Training(dataset):
     
   ##### Normalization 
-  Normalization_Type = 'contrast_norm'
-  # Normalization_Type = 'same' 
+  #Normalization_Type = 'contrast_norm'
+  Normalization_Type = 'same' 
+  # Normalization_Type = tf.keras.layers.BatchNormalization
   
   ##### Choosing Dataset Type
   for i in range(len(dataset)-1):
@@ -41,53 +42,50 @@ def Model_Training(dataset):
   Number_of_Filters = 32
   Latent_Dimension = 100
   Visible_Distribution = 'cont_bernoulli'
-
+  
+  print("Name of the Dataset",str(dataset_name))
   print("Batch Size:",Batch_Size)
   print("Distribution:",Visible_Distribution)
   print("Number of Filters:",Number_of_Filters)
-  print("cont_bernoulli:",Visible_Distribution)
+  print("Number of Channels:",Channels)
   print(" ")
                                         
   ##### Fetching Training, Validation & Testing Dataset                                    
-  Training, Validation, Testing = dataset_prep.get_dataset(dataset_name, Batch_Size,mode,normalize=None,dequantize=False,visible_dist=Visible_Distribution)
+  Training, Validation = dataset_prep.get_dataset(str(dataset_name), Batch_Size,mode,normalize=None,dequantize=False,shuffle_train=True,visible_dist=Visible_Distribution)
   
   ##### Fetching Variational Autoencoder Model
   Variational_Autoencoder = CVAE_Network.CVAE(input_shape=(Number_of_Filters, Number_of_Filters, Channels),num_filters=Number_of_Filters,latent_dim=Latent_Dimension,visible_dist=Visible_Distribution)
  
   ##### Optimizing & Compiling to imrpove the Model's Accuracy and reduce the Loss
+  Directory_Model = os.path.join('test' , f'Models\{dataset_name}\{mode}\{Visible_Distribution}')
+  checkpoint_path = os.path.join(Directory_Model, 'weights.hdf5')
+  
   Optimizer = tf.optimizers.Adam(learning_rate=5e-4)
   Variational_Autoencoder.compile(optimizer=Optimizer,loss={'KL Divergence Loss': Variational_Autoencoder.kl_divergence_loss,'Decoder Loss': Variational_Autoencoder.decoder_nll_loss})
+  Variational_Autoencoder.save_weights(filepath=checkpoint_path)
   
   ##### Initializing the Training & Evaluation
-  start_training= True
-  start_evaluation = True
-  Epochs=1
-  Directory_Model = os.path.join('test' , f'Models\{dataset_name}\{mode}\{Visible_Distribution}')
-  Directory_Log = os.path.join('test' , 'logs',  f'Models\{dataset_name}\{mode}\{Visible_Distribution}')
+  
+  Epochs=1# 50
 
-  if start_training:
-    tf.io.gfile.makedirs(Directory_Model)
-    callbacks = [Supporting_Function.TensorBoardWithLLStats(Evaluation_Every, dataset_name,dataset_name, mode, Normalization_Type, Visible_Distribution,log_dir=Directory_Log,
-            update_freq='epoch'), tf.keras.callbacks.ModelCheckpoint(filepath=os.path.join(Directory_Model, 'weights.hdf5'),verbose=1, save_weights_only=True, save_best_only=True)]
-
-    print("Initializing the Training")
-    Variational_Autoencoder.fit(Training,epochs=Epochs,validation_data=Validation,callbacks=callbacks)
-    # test\Models\fashion_mnist\grayscale\1\weights.hdf5
-  if start_evaluation:
-    weights = tf.io.gfile.listdir(Directory_Model).sort()
-    Variational_Autoencoder.build([None]+list((Number_of_Filters, Number_of_Filters, Channels)))
+  print("Initializing the Training")
+  # Creating a callback to save the model's weights only during training
+  callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,save_weights_only=True,verbose=1)
+  Variational_Autoencoder.fit(Training,epochs=Epochs,validation_data=Validation,callbacks=[callback])
     
-    weights_path = os.path.join(Directory_Model,weights[-1])
-    Variational_Autoencoder.load_weights(weights_path)
-
-    Variational_Autoencoder.compute_corrections(Training)
+  print("Initializing the Evaluation")
+  #weights = tf.io.gfile.listdir(Directory_Model).sort()
+  Variational_Autoencoder.build([None]+list((Number_of_Filters, Number_of_Filters, Channels)))
     
-    print("Initializing the Evaluation")
-    Variational_Autoencoder.evaluate(Validation)
-    probs_res = Supporting_Function.get_probs(dataset_name, Variational_Autoencoder ,mode,Normalization_Type, n_samples=5,split='test',training=False,visible_dist=Visible_Distribution)
+  Variational_Autoencoder.load_weights(checkpoint_path)
+    
+  Variational_Autoencoder.compute_corrections(Training)
+    
+  Variational_Autoencoder.evaluate(Validation)
+  probs_res = Supporting_Function.get_probs(dataset_name, Variational_Autoencoder ,mode,Normalization_Type, n_samples=5,split='test',training=False,visible_dist=Visible_Distribution)
                       
-    with tf.io.gfile.GFile(os.path.join(Directory_Model, 'probs.pkl'), 'wb') as f:
-      pickle.dump(probs_res, f)
+  with tf.io.gfile.GFile(os.path.join(Directory_Model, 'probs.pkl'), 'wb') as f:
+     pickle.dump(probs_res, f)
 
 if __name__ == '__main__':
     
